40c5c01aea3ce5ce894afa364c6793fa0162b629
Brian Rainey <raineyb@gmail.com>
2014-08-29 11:23:01 -0400

    Fix path for Markdown

--- init.el
+++ init.el
@@ -72,6 +72,7 @@
     (message buffer-file-name)))
 
 ;; Give it a key binding
+(global-unset-key (kbd "C-c C-p"))
 (global-set-key (kbd "C-c C-p") 'copy-path)
 
 ;; dash.el
@@ -95,6 +96,8 @@
 (require 'cider)
 
 ;; markdown-mode
-(autoload 'markdown-mode "markdown-mode/markdown-mode" "Major mode for editing Markdown files" t)
+(add-to-list 'load-path "~/.emacs.d/packages/markdown-mode")
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
