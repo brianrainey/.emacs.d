710f43d21a10f41a3da9a15b7dea2d1e72fe11af
Brian Rainey <brian@rainey.us>
2015-08-12 13:38:20 -0500

    Add proxy configuration

--- init-files/init-packages.el
+++ init-files/init-packages.el
@@ -1,4 +1,10 @@
 ;; Melpa package repository
+
+;; If the OS has an environment variable for the proxy server, use it.
+(let '(proxy (getenv "HTTP_PROXY"))
+  (when (not (null proxy))
+    (setq url-proxy-services '("http" . proxy))))
+
 (require 'package)
 (add-to-list 'package-archives
   '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/") t)
