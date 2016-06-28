f62bc76d567f4d467f11a275fb685f9bfbdbfc1a
Brian Rainey <brian@rainey.us>
2016-06-28 09:13:30 -0500

    Free up C-t for tmux.

--- init-files/init-terminal.el
+++ init-files/init-terminal.el
@@ -1,4 +1,5 @@
 ;; Terminal only settings
 (when (eq window-system nil)
   (load-theme 'misterioso t)
-  (set-face-background 'highlight "color-235"))
+  (set-face-background 'highlight "color-235")
+  (global-unset-key (kbd "C-t")))
